92d910ae8df48851d0fa1f63022c404c7e31ac6b
Haydn Appleby <haydn@commonplace.is>
2022-02-03 10:24:37 +0000

    add sentry logging to the custom error handler pages

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,11 +1,13 @@
 import type { NextPage } from "next";
 import { useRouter } from "next/router";
 import Error from "next/error";
+import * as Sentry from "@sentry/nextjs";
 
 const Custom404: NextPage = () => {
   const router = useRouter();
   console.log(router);
   // Dispatch error, message, status etc. on Sentry, Mixpanel...
+  Sentry.captureMessage(`This is a 404 on route: ${router.asPath}`);
 
   return <Error statusCode={404} />;
 };
--- pages/500.tsx
+++ pages/500.tsx
@@ -1,11 +1,13 @@
 import type { NextPage } from "next";
 import { useRouter } from "next/router";
 import Error from "next/error";
+import * as Sentry from "@sentry/nextjs";
 
 const Custom500: NextPage = () => {
   const router = useRouter();
   console.log(router);
   // Dispatch error, message, status etc. on Sentry, Mixpanel...
+  Sentry.captureMessage(`This is a 500 on route: ${router.asPath}`);
 
   return <Error statusCode={500} />;
 };
