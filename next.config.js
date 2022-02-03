9555f62af96dcde57f44121b949e850638ad2b84
Haydn Appleby <haydn@commonplace.is>
2022-02-03 11:23:06 +0000

    update next config to use with sentry

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
+
+const { withSentryConfig } = require("@sentry/nextjs");
+
 const nextConfig = {
   i18n: {
     locales: ["default", "en", "de", "fr"],
@@ -6,7 +9,19 @@ const nextConfig = {
     localeDetection: false,
   },
   reactStrictMode: true,
-  // trailingSlash: true,
+  trailingSlash: true,
 };
 
-module.exports = nextConfig;
+const sentryWebpackPluginOptions = {
+  // Additional config options for the Sentry Webpack plugin. Keep in mind that
+  // the following options are set automatically, and overriding them is not
+  // recommended:
+  //   release, url, org, project, authToken, configFile, stripPrefix,
+  //   urlPrefix, include, ignore
+
+  silent: true, // Suppresses all logs
+  // For all available options, see:
+  // https://github.com/getsentry/sentry-webpack-plugin#options.
+};
+
+module.exports = withSentryConfig(nextConfig, sentryWebpackPluginOptions);
